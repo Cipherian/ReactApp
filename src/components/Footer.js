0129caef4cdd97557a8d128282f13863a8a03771
Daniel Brott <danbrott42@gmail.com>
2022-07-16 21:26:19 -0700

    first commit, got most things except stretch goal

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+class Footer extends Component {
+  render(){
+    return(
+      <p>Author: Daniel Brott</p>
+    )
+  }
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <h1>Beastly Animals with Horns</h1>
+    )
+  }
+}
+
+export default Header;
