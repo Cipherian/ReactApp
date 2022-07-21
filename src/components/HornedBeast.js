3747e3f38a943c747e491adfb0bba40f5bba3d07
Daniel Brott <danbrott42@gmail.com>
2022-07-20 17:19:01 -0700

    updated with json, click handler not working yet

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,21 +1,21 @@
 import { Component } from "react";
 
+
+
 class HornedBeast extends Component {
-  constructor(props) {
-    super();
-  }
   render() {
     return (
-      <div id="horned">
-        <h2>{this.props.title}</h2>
-        <img
-          src={this.props.imageUrl}
-          alt={this.props.alt}
-          title={this.props.title}
-        />
-        <p>{this.props.description}</p>
+      <div class="beast-list">
+        {this.props.beasts.map((beast) => (
+          <img
+            alt=""
+            src={beast.image_url}
+            onClick={this.beastClicks}
+          />
+        ))}
       </div>
     );
   }
 }
+
 export default HornedBeast;
